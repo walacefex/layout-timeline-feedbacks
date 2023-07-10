167ae8be4f6ac06d5497a06d866e219a7561126f
walacefex <walacefex01@gmail.com>
2023-07-10 20:50:46 -0300

    Iterating with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,35 @@ import { Post } from './components/Post';
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [{
+  id: 1,
+  author:{
+    avatarUrl: 'https://github.com/walacefelix.png',
+    name: 'Walace Felix',
+    role: 'Software Engineer'
+  },
+  content: [
+   { type: 'paragraph', content: 'Fala galeraa 👋'},
+   { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'jane.design/doctorcare'},
+  ], 
+  publishedAt: new Date('2023-07-10 20:00:00'),
+},
+{
+  id: 2,
+  author:{
+    avatarUrl: 'https://github.com/UnicornCoderBR.png',
+    name: 'Diego Telles',
+    role: 'Software Engineer'
+  },
+  content: [
+   { type: 'paragraph', content: 'Fala galeraa 👋'},
+   { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'jane.design/doctorcare'},
+  ], 
+  publishedAt: new Date('2023-07-09 20:00:00'),
+}]
+
 export function App() {
   return (
     <div>
@@ -12,7 +41,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author={'Walace Felix'} content={'Desenvolvedo Front-end'} />
+         {posts.map(post => {
+          return (
+          <Post 
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          />)
+         })}
         </main>
       </div>
     </div>

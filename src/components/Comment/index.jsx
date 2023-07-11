ad6fedf717ebecb5ab6875f525b62b34f6d52604
walacefex <walacefex01@gmail.com>
2023-07-10 22:55:42 -0300

    Creating the function to like in the comments

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { Trash, ThumbsUp } from 'phosphor-react';
 import { Avatar } from '../Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
   }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/walacefex.png" />
@@ -26,9 +34,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>

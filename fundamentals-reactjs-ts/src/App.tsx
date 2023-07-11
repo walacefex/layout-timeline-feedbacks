c7c233f8ca27f24f353decde37208a6ee258e6cd
walacefex <walacefex01@gmail.com>
2023-07-11 00:04:29 -0300

    adding project with typescript

---
+++ fundamentals-reactjs-ts/src/App.tsx
@@ -0,0 +1,55 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post, PostType } from './components/Post';
+
+import './global.css';
+import styles from './App.module.css';
+
+const posts: PostType[] = [{
+  id: 1,
+  author:{
+    avatarUrl: 'https://github.com/walacefex.png',
+    name: 'Walace Felix',
+    role: 'Software Engineer'
+  },
+  content: [
+   { type: 'paragraph', content: 'Fala galeraa 👋'},
+   { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'jane.design/doctorcare'},
+  ], 
+  publishedAt: new Date('2023-07-10 20:00:00'),
+},
+{
+  id: 2,
+  author:{
+    avatarUrl: 'https://github.com/UnicornCoderBR.png',
+    name: 'Diego Telles',
+    role: 'Software Engineer'
+  },
+  content: [
+   { type: 'paragraph', content: 'Fala galeraa 👋'},
+   { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    { type: 'link', content: 'jane.design/doctorcare'},
+  ], 
+  publishedAt: new Date('2023-07-09 20:00:00'),
+}]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+         {posts.map((post) => {
+          return (
+          <Post 
+            key={post.id}
+            post={post}
+          />)
+         })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ fundamentals-reactjs-ts/src/components/Comment/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { Trash, ThumbsUp } from 'phosphor-react';
+import { Avatar } from '../Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (content: string) => void;
+}
+
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((stateLikeCount) => {
+      return  stateLikeCount + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/walacefex.png" alt="Walace Felix" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Walace Felix</strong>
+              <time title="07 de Junho às 18:00h" dateTime="2022-06-07 18:00:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}

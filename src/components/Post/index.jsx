3a4da6348a917eef0e42c47e695b6f30bb1826cc
walacefex <walacefex01@gmail.com>
2023-07-10 22:50:49 -0300

    Validating a form using methods to not post comments with no input value.

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -26,8 +26,13 @@ export function Post({ author, content, publishedAt}) {
   }
 
   function handleNewCommentChange(){
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
 
+  }
+  
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Este campo é obrigatório');
   }
 
   function deleteComment(commentToDelete) {
@@ -36,6 +41,9 @@ export function Post({ author, content, publishedAt}) {
     });
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
 
   return (
     <article className={styles.post}>
@@ -70,9 +78,11 @@ export function Post({ author, content, publishedAt}) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">
+          <button type="submit" disabled={isNewCommentEmpty}>
             Publicar
           </button>
         </footer>
